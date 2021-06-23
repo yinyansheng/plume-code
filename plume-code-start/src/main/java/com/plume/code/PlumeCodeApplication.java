7855eb560fb072a8f437c9c62396b77aab202f55
yinyansheng <yys5008574@ixiancheng.com>
2021-06-23 20:41:06 +0800

    m

---
+++ plume-code-start/src/main/java/com/plume/code/PlumeCodeApplication.java
@@ -0,0 +1,13 @@
+package com.plume.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlumeCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlumeCodeApplication.class, args);
+    }
+
+}
---
+++ plume-code-start/src/test/java/com/plume/code/PlumeCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plume.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlumeCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
